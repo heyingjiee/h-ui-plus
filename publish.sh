fc962b30d2fba9264c5cd271b5b5605b57956dc3
何英杰 <heyingjie@yiche.com>
2023-09-18 14:11:23 +0800

    feat: 发布脚本修改

--- publish.sh
+++ publish.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+#设置为官方源
+npm config set registry=https://registry.npmjs.org
 
-#npm config get registry # 检查仓库镜像库
-npm config set //registry.npmjs.org/:_authToken "$NPN_AUTH_TOKEN" #设置为官方源
-#echo '请进行登录相关操作：'
-#npm login # 登陆
+# $NPN_AUTH_TOKEN是npm的token
+# 使用 -z 选项检查环境变量是否为空或未定义
+if [ -z "$NPN_AUTH_TOKEN" ]; then
+    #未定义
+    echo '请进行登录相关操作：'
+    npm login # 登陆
+else
+  #已定义（Github Action传入了）
+   npm config set //registry.npmjs.org/:_authToken "$NPN_AUTH_TOKEN"
+fi
+
 echo "-------publishing-------"
 npm publish # 发布
 # npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
