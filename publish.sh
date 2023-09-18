9579d14a590ce95c11826f8005a23ff676ad3610
何英杰 <heyingjie@yiche.com>
2023-09-18 13:33:00 +0800

    chore: 修改发布脚本

--- publish.sh
+++ publish.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+
 npm config get registry # 检查仓库镜像库
-npm config set registry=https://registry.npmjs.org/:_authToken $NPN_AUTH_TOKEN #设置为官方源
-echo '请进行登录相关操作：'
-npm login # 登陆
+npm config set registry=https://registry.npmjs.org/:_authToken "$NPN_AUTH_TOKEN" #设置为官方源
+#echo '请进行登录相关操作：'
+#npm login # 登陆
 echo "-------publishing-------"
 npm publish # 发布
 # npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
-echo "发布完成"
+if [ $? -eq 0 ]; then
+    echo "发布成功"
+else
+    echo "发布失败"
+fi
+
 exit
