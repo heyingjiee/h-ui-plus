e313e36dadb6cafd4d2059512774efb77ef1f9af
何英杰 <heyingjie@yiche.com>
2023-09-18 13:45:29 +0800

    chore: 修改脚本命令

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-npm config get registry # 检查仓库镜像库
-npm config set registry=https://registry.npmjs.org/:_authToken "$NPN_AUTH_TOKEN" #设置为官方源
+#npm config get registry # 检查仓库镜像库
+npm config set //registry.npmjs.org/:_authToken "$NPN_AUTH_TOKEN" #设置为官方源
 #echo '请进行登录相关操作：'
 #npm login # 登陆
 echo "-------publishing-------"
