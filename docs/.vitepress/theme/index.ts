630a7466c2b9bbdf3e1629853ab8073855f31a9d
何英杰 <heyingjie@yiche.com>
2023-09-20 01:36:40 +0800

    chore: 修改base

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,9 +2,9 @@ import DefaultTheme from 'vitepress/theme'
 // import HUI from '../../../src/entry'
 import './style/var.css'
 export default {
+  extends: DefaultTheme,
   //发布在github page，地址是<username>.github.io/h-ui-plus
   base: '/h-ui-plus/',
-  extends: DefaultTheme,
   // enhanceApp(ctx) {
     // register your custom global components
     // ctx.app.component('JSButton', JSButton)
