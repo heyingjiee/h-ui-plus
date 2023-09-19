94025a9df1c94a6cad5d0ae495306e5fd75bbf37
何英杰 <heyingjie@yiche.com>
2023-09-20 00:38:39 +0800

    chore: 发布文档地址

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,8 @@ import DefaultTheme from 'vitepress/theme'
 // import HUI from '../../../src/entry'
 import './style/var.css'
 export default {
+  //发布在github page，地址是<username>.github.io/h-ui-plus
+  base: '/h-ui-plus',
   extends: DefaultTheme,
   enhanceApp(ctx) {
     // register your custom global components
