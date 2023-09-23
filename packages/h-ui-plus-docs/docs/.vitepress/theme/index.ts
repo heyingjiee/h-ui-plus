bc4d3be52270efc89213a7fbbef8c765f2c9c425
何英杰 <heyingjie@yiche.com>
2023-09-23 21:58:13 +0800

    feat: 修改引入组件文件

--- packages/h-ui-plus-docs/docs/.vitepress/theme/index.ts
+++ packages/h-ui-plus-docs/docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import Theme from 'vitepress/theme'
-import HUI from 'h-ui-plus-components'
+import HUI from 'h-ui-plus-components/dist/h-ui'
 import 'h-ui-plus-components/dist/entry.css'
 import './style/var.css'
 
--- packages/h-ui-plus-docs/vite.config.ts
+++
@@ -1,7 +0,0 @@
-// import { defineConfig } from "vite";
-// import Unocss from "../../h-ui-plus-components/config/unocss";
-// // https://vitejs.dev/config/
-//
-// export default defineConfig({
-//   plugins: [Unocss()],
-// });
