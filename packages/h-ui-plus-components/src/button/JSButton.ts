68d6619a8fb4ea86e4e7337b81d7f2d59f8eba0d
何英杰 <heyingjie@yiche.com>
2023-09-28 18:02:37 +0800

    chore: 修改包版本

--- packages/h-ui-plus-components/src/button/JSButton.ts
+++ packages/h-ui-plus-components/src/button/JSButton.ts
@@ -3,6 +3,6 @@ import { defineComponent, h } from "vue";
 export default defineComponent({
   name: "JSButton",
   render() {
-    return h("button", null, "JS按钮文案");
+    return h("button", null, "JS按钮文案-1");
   },
 });
