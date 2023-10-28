bf60e9033b0ca5ae5cc29e272d5195d6a47ad47f
何英杰 <heyingjie@yiche.com>
2023-10-28 16:05:49 +0800

    feat: 保留SFC组件

--- packages/h-ui-plus-components/src/Button/__tests__/Button.spec.ts
+++ packages/h-ui-plus-components/src/Button/__tests__/Button.spec.ts
@@ -6,7 +6,7 @@ import { describe, expect, test } from "vitest";
 //分组
 describe("Button分组", () => {
   //测试用例
-  test("加载TSX组件，对比组件文本", () => {
+  test("加载TSX组件,对比组件文本", () => {
     const wrapper = shallowMount(TSXButton);
 
     expect(wrapper.text()).toBe("TSXButton组件");
--- packages/h-ui-plus-components/src/Button/index.ts
+++ packages/h-ui-plus-components/src/Button/index.ts
@@ -1,17 +1,13 @@
 import { App } from "vue";
 
-import JSButton from "./src/JSButton";
-import TSXButton from "./src/TSXButton";
 import SFCButton from "./src/SFCButton.vue";
 
 import "uno.css";
 
 export default {
   install(app: App): void {
-    app.component(JSButton.name, JSButton);
-    app.component(TSXButton.name, TSXButton);
     app.component(SFCButton.__name, SFCButton);
   },
 };
 
-export { JSButton, SFCButton, TSXButton };
+export { SFCButton };
