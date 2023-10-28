fc72d731c0ac514875adb46641f79c01634af075
何英杰 <heyingjie@yiche.com>
2023-10-28 16:07:28 +0800

    feat: 修改测试用例

--- packages/h-ui-plus-components/src/Button/__tests__/Button.spec.ts
+++ packages/h-ui-plus-components/src/Button/__tests__/Button.spec.ts
@@ -1,4 +1,4 @@
-import TSXButton from "../index";
+import SFCButton from "../index";
 
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
@@ -6,9 +6,9 @@ import { describe, expect, test } from "vitest";
 //分组
 describe("Button分组", () => {
   //测试用例
-  test("加载TSX组件,对比组件文本", () => {
-    const wrapper = shallowMount(TSXButton);
+  test("加载SFCButton组件,对比组件文本", () => {
+    const wrapper = shallowMount(SFCButton);
 
-    expect(wrapper.text()).toBe("TSXButton组件");
+    expect(wrapper.text()).toBe("SFC按钮文案");
   });
 });
